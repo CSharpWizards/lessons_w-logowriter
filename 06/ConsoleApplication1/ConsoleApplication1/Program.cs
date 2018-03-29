4d897f189b3e59f5072cdcb5387ddee123b1e63b
logowriter <lubovskiy.nick@gmail.com>
2018-03-29 20:25:40 +0300

    заяц

---
+++ 06/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine(" введите имя вашего зайца ");
+            String name = Console.ReadLine();
+            Console.WriteLine("вашего зайца зовут " + name + " вот он ");
+            Console.WriteLine("(\\___/)");
+            Console.WriteLine("(=\'.\'=)");
+            Console.WriteLine("(\") (\")");
+            Console.ReadLine();
+        }
+
+    }
+}
