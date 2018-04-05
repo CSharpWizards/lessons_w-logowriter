3ee1333f9deb6ad9c25fd390dba669ac9178eef7
Alex <iseldomused@gmail.com>
2018-04-05 13:55:12 +0300

    add 08

---
+++ 08/00_formatting/Program.cs
@@ -0,0 +1,16 @@
+using System;
+    using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tutorial {
+    class Program
+    {
+        static void Main(string[] args) {
+        Console.WriteLine("Введите ваше имя:");
+        string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + "!");
+            Console.WriteLine("Нажмите ENTER что бы выйти из программы");
+                Console.Read();}
+    }
+    }
---
+++ 08/01_product/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Tutorial {
+    public static void Main() {
+        // пишите свой код здесь
+
+        // тестовый код
+        Console.WriteLine("productName: " + productName);
+        Console.WriteLine("productYear: " + productYear);
+        Console.WriteLine("productPrice: " + productPrice);
+
+        Console.ReadLine();
+    }
+}
