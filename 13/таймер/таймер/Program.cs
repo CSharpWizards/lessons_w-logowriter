a2c4b0f7d2535121a6979b39d89573ad6f7c920a
logowriter <lubovskiy.nick@gmail.com>
2018-05-17 20:29:32 +0300

    13

---
+++ 13/таймер/таймер/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("со старта программы прошло :");
+            int second = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                second = second + 1;
+                Console.WriteLine(second);
+            }
+
+        }
+    }
+}
